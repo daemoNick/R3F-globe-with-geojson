3f87314c657075eaac6fb77e94485d7bd2177c71
Dominic Essien <essiendominic1@gmail.com>
2023-10-22 23:36:02 +0100

    app component: set camera position

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,35 @@
+import React, { Suspense } from "react";
 import { OrbitControls } from "@react-three/drei";
+import Globe from "./Globe";
+import GeoJSON from "./GeoJSON";
+import { useThree } from "@react-three/fiber";
+
+const PointLight = () => {
+  return <pointLight color={"white"} intensity={1} position={[10, 10, 10]} />;
+};
 
 export const Experience = () => {
+  // const { camera } = useThree();
+
+  // function logCameraPosition() {
+  //   console.log(camera);
+  // }
+  // window.addEventListener("click", logCameraPosition);
+
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight color="lightblue" />
+      <PointLight />
+      <Suspense fallback={null}>
+        <Globe
+          map="/earth-large.jpg"
+          bump="bump-large.jpg"
+          clouds="/clouds-large.jpg"
+        />
+        <GeoJSON />
+      </Suspense>
+      <OrbitControls />
     </>
   );
 };
